00b1e705e7d41e60df084dbbfbfe4ca51e5a1489
Craig Riecke <cr396@cornell.edu>
2015-12-11 16:21:39 -0500

    More generalization

--- coscin_switch_app.py
+++ coscin_switch_app.py
@@ -29,29 +29,17 @@ class CoscinSwitchApp(frenetic.App):
   STATE_NORMAL_OPERATION = 2
   state = STATE_INITIAL_CONFIG
 
-  # This controller instance can handle either switch separately, or both in one.  We
-  # keep track of which switch(es) were handling through these Booleans
-  connected_to_ithaca = false
-  connected_to_nyc = false
-
   switches = {}
 
-  # In later incarnations, this will get dynamically changed to 0, 1 or 2
-  # depending on the utilization.  The paths for now are:
-  #  0:  192.168.156.* -> 192.168.157.*
-  #  1:  192.168.158.* -> 192.168.159.*
-  #  2:  192.168.160.* -> 192.168.161.*
-
-  paths = [ 
-    ("192.168.156", "192.168.157"),
-    ("192.168.158", "192.168.159"),
-    ("192.168.160", "192.168.161")
-  ]
+  # index of alternate_path being used now
   current_path = 0
 
   # hosts = { mac1 => (sw1, port1), mac2 => (sw2, port2), ... }
-  # where hoplistn is the next_hop table for each switch: [(sw1, port1), (sw2, port2), etc.] 
   hosts = {}
+
+  # endhosts = { "ithaca" => [(host_portion_of_ip), ...], "nyc" => ... }
+  # This is more for enumerating end user hosts on each end network
+  endhosts = { "ithaca": [], "nyc": []}
 
   # We maintain our own little ARP table for the router IP's
   arp_cache = {}
@@ -92,45 +80,81 @@ class CoscinSwitchApp(frenetic.App):
     return ipv4_to_str(ip_int)
     
   # Send payload to all ports  
-  def flood_indiscriminately(self, dpid, payload):
-    output_actions = [ Output(Physical(p)) for p in self.switches[self.dpid_to_switch(dpid)] ]
+  def flood_indiscriminately(self, switch, payload):
+    output_actions = [ Output(Physical(p)) for p in self.switches[switch] ]
     # Only bother to send the packet out if there are ports to send it out on.
     if output_actions:
-      self.pkt_out(dpid, payload, output_actions)
+      self.pkt_out(self.switch_to_dpid(switch), payload, output_actions)
 
-  def send_arp_request(self, dpid, src_ip, target_ip):
+  def arp_payload(e, pkt):
+    p = packet.Packet()
+    p.add_protocol(e)
+    p.add_protocol(pkt)
+    p.serialize()
+    return NotBuffered(binascii.a2b_base64(binascii.b2a_base64(p.data)))
+
+  def send_arp_request(self, switch, src_ip, target_ip):
     # It's unclear what the source should be, since the switch has no mac or IP address.
     # It just hears all replies and picks out the interesting stuff.
     src_mac = "00:de:ad:00:be:ef"
     dst_mac = "ff:ff:ff:ff:ff:ff"
     e = ethernet.ethernet(dst=dst_mac, src=src_mac, ethertype=ether.ETH_TYPE_ARP)
     pkt = arp.arp_ip(arp.ARP_REQUEST, src_mac, src_ip, "00:00:00:00:00:00", target_ip)
-    p = packet.Packet()
-    p.add_protocol(e)
-    p.add_protocol(pkt)
-    p.serialize()
-    payload = NotBuffered(binascii.a2b_base64(binascii.b2a_base64(p.data)))
+    payload = arp_payload(e, pkt)
     logging.info("Sending Arp Request to "+target_ip)
-    self.flood_indiscriminately(dpid, payload)
+    self.flood_indiscriminately(self.switch_to_dpid(switch), payload)
 
-  def send_arp_request_router_interface(self, dpid, target_ip_net):
+  def send_arp_request_router_interface(self, switch, target_ip_net):
     # Note this may not or may not be a real host, but the reply will always come back to the switch anyway.
     src_ip = ip_for_network(target_ip_net, 250)
     dst_ip = ip_for_network(target_ip_net, 1)  # The IP of the router interface will always be a .1
-    self.send_arp_request(dpid, src_ip, dst_ip)
+    self.send_arp_request(switch, src_ip, dst_ip)
 
-  def arp_reply(self, dpid, port_id, src_mac, src_ip, target_mac, target_ip):
+  def arp_reply(self, switch, port_id, src_mac, src_ip, target_mac, target_ip):
     # Not sure if the src is right.  We should be answering "us", as in the switch,
     # but the mac address of "us" is unclear.  
     e = ethernet.ethernet(dst=src_mac, src=target_mac, ethertype=ether.ETH_TYPE_ARP)
     # Note for the reply we flip the src and target, as per ARP rules
     pkt = arp.arp_ip(arp.ARP_REPLY, target_mac, target_ip, src_mac, src_ip)
-    p = packet.Packet()
-    p.add_protocol(e)
-    p.add_protocol(pkt)
-    p.serialize()
-    payload = NotBuffered(binascii.a2b_base64(binascii.b2a_base64(p.data)))
-    self.pkt_out(dpid, payload, [Output(Physical(port_id))])
+    payload = arp_payload(e, pkt)
+    self.pkt_out(self.switch_to_dpid(switch), payload, [Output(Physical(port_id))])
+
+  def send_to_controller():
+    Mod(Location(Pipe("coscin_switch_app")))
+
+  def is_arp():
+   Test(EthType(0x806))
+
+  def at_ithaca():
+    Test(Switch(self.switch_to_dpid("ithaca")))
+
+  def at_nyc():
+    Test(Switch(self.switch_to_dpid("nyc")))
+
+  def dest_is(ip_net, ip_host):
+    Test(IP4Dst(ip_for_network(ip_net, ip_host)))
+
+  def dest_real_net(switch, ip_host):
+    dest_is(self.config[switch]["network"], ip_host)
+ 
+  ########################
+  # Frenetic Dispatchers
+
+  def connected(self):
+    # If Frenetic went down, it'll call connected() when it comes back up.  In that case, just 
+    # resend the current policies
+    if self.state == self.STATE_NORMAL_OPERATION:
+      self.update(self.policy())
+    else:
+      self.current_switches(callback=handle_current_switches) 
+
+  def packet_in(self, dpid, port_id, payload):
+    if self.state == self.STATE_INITIAL_CONFIG:
+      logging.info("Packets received before initialization, dropping" )
+    elif self.state == self.STATE_ROUTER_LEARNING:
+      self.packet_in_router_learning(dpid, port_id, payload)
+    else:
+      self.packet_in_normal_operation(dpid, port_id, payload)
 
   ########################
   # Initial Config Mode
@@ -144,8 +168,6 @@ class CoscinSwitchApp(frenetic.App):
     logging.info("Pausing 2 seconds to allow rules to be installed")
     IOLoop.instance().add_timeout(datetime.timedelta(seconds=2), self.learn_routers)
 
-
-
   ########################
   # Router Learning Mode
   # We assume all router interfaces are up for all alternate paths and that they're fixed for the duration
@@ -156,69 +178,66 @@ class CoscinSwitchApp(frenetic.App):
     pkt = packet.Packet(array.array('b', payload.data))
     p = get(pkt, 'arp')
     switch = self.dpid_to_switch(dpid)
-    logging.info("Received "+p.src_ip+" -> ("+str(switch)+", "+str(port_id)+") -> "+p.dst_ip)
+    logging.info("Received "+p.src_ip+" -> ("+switch+", "+str(port_id)+") -> "+p.dst_ip)
     self.arp_cache[p.src_ip] = p.src_mac
-    if len(self.arp_cache) >= len(self.paths) * 2 + 2:
-      logging.info("Switching to Normal mode" )
-      self.update(self.normal_operation_policy())
-      self.state = self.STATE_NORMAL_OPERATION
-
+    if len(self.arp_cache) >= len(self.config.alternate_paths) * len(self.switches):
+      self.normal_mode()
 
   def router_learning_policy(self):
     # In the intial config, grab all ARP replies for ourselves
-    return Filter( \
-      Test(EthType(0x806)) 
-      # TODO: We may want to filter only replies
-    ) >> Mod(Location(Pipe("coscin_switch_app")))
+    return Filter( is_arp() ) >> send_to_controller()
 
   def learn_routers(self):
     logging.info("Switching to Router Learning mode")
     self.state = self.STATE_ROUTER_LEARNING
     # Send out ARP packets for all router interfaces to learn their mac addresses.  The
     # packet_in_router will handle the replies as they arrive
-    for path in self.paths:
-      ithaca_router_net = path[0]
-      self.send_network_arp_request(self.switch_to_dpid("ithaca"), ithaca_router_net)
-      nyc_router_net = path[1] 
-      self.send_network_arp_request(self.switch_to_dpid("nyc"), nyc_router_net)
+    for path in self.config.alternate_paths:
+      if "ithaca" in self.config:
+        ithaca_router_net = path["ithaca"]
+        self.send_arp_request_router_interface("ithaca", ithaca_router_net)
+      if "nyc" in self.config:
+        nyc_router_net = path["nyc"] 
+        self.send_arp_request_router_interface("nyc", nyc_router_net)
     # TODO: This is a little discomforting, as it requires hosts to be up when app is
     # started.  Might need to do a little ARP spoofing.  
-    self.send_arp_request(self.switch_to_dpid("ithaca"), "192.168.156.250", "192.168.56.100")
-    self.send_arp_request(self.switch_to_dpid("nyc"), "192.168.157.250", "192.168.57.100")
+    # self.send_arp_request(self.switch_to_dpid("ithaca"), "192.168.156.250", "192.168.56.100")
+    # self.send_arp_request(self.switch_to_dpid("nyc"), "192.168.157.250", "192.168.57.100")
 
-  def connected(self):
-    # If Frenetic went down, it'll call connected() when it comes back up.  In that case, just 
-    # resend the current policies
-    if self.state == self.STATE_NORMAL_OPERATION:
-      self.update(self.policy())
-    else:
-      self.current_switches(callback=handle_current_switches) 
+  ########################
+  # Normal Mode
+  # This basically handles all traffic afterwards.  It learns host ports coming up.  
+
+  def normal_mode(self):
+    logging.info("Switching to Normal mode" )
+    self.update(self.normal_operation_policy())
+    self.state = self.STATE_NORMAL_OPERATION
 
   def capture_arp_requests_policy(self):
-    # ARP packets looking for the default gateway 56.1 will be occurring right away.
-    # The 56.1 interface is virtual, and we map it to either 156.1, 158.1 or 160.1 
-    # depending on the state.  
-    ithaca_outgoing = Filter( \
-      Test(Switch(self.switch_to_dpid("ithaca"))) & \
-      Test(EthType(0x806)) &
-      Test(IP4Dst("192.168.56.1"))
-    ) >> Mod(Location(Pipe("coscin_switch_app")))
-    nyc_incoming = Filter( \
-      Test(Switch(self.switch_to_dpid("nyc"))) & \
-      Test(EthType(0x806)) &
-      Test(IP4Dst("192.168.157.100"))
-    ) >> Mod(Location(Pipe("coscin_switch_app")))
-    nyc_outgoing = Filter( \
-      Test(Switch(self.switch_to_dpid("nyc"))) & \
-      Test(EthType(0x806)) &
-      Test(IP4Dst("192.168.57.1"))
-    ) >> Mod(Location(Pipe("coscin_switch_app")))
-    ithaca_incoming = Filter( \
-      Test(Switch(self.switch_to_dpid("ithaca"))) & \
-      Test(EthType(0x806)) &
-      Test(IP4Dst("192.168.156.100"))
-    ) >> Mod(Location(Pipe("coscin_switch_app")))
-    return Union([ ithaca_outgoing, nyc_incoming, nyc_outgoing, ithaca_incoming ])
+
+    # In normal mode, we capture ARP requests for IP's that don't really exist.  You can
+    # think of them as symbolic links to the real IP.  First, we capture .1 address of
+    # each of the endpoint networks
+
+    policies = []
+    policies << Filter(at_ithaca() & is_arp() & dest_real_net("ithaca", 1)) >> send_to_controller()
+    policies << Filter(at_nyc() & is_arp() & dest_real_net("nyc", 1)) >> send_to_controller()
+
+    # We also capture virtual host addresses for each side of the alternate path
+    # These will get mapped to their actual host address once they've travelled through 
+    # the NYC<->Ithaca path and reach their destination net
+
+    for host in self.endhosts["ithaca"]:
+      for path in self.config.alternate_paths:
+        dest_net = path["ithaca"]
+        policies << Filter( at_nyc() & is_arp() & dest_is(dest_net, host)) >> send_to_controller()
+
+    for host in self.endhosts["nyc"]:
+      for path in self.config.alternate_paths:
+        dest_net = path["nyc"]
+        policies << Filter( at_ithaca() & is_arp() & dest_is(dest_net, host)) >> send_to_controller()
+
+    return policies
 
   def preferred_route_policy(self, switch_id, port_id, dest_ip):
     # TODO: renumber the xx network to the 1xx network, leaving host in place
@@ -285,14 +304,6 @@ class CoscinSwitchApp(frenetic.App):
       self.direct_route_policy("ithaca",2,"192.168.157.100","192.168.156.100"),
       self.direct_route_policy("nyc",2,"192.168.156.100","192.168.157.100")
     ])
-
-  def packet_in(self, dpid, port_id, payload):
-    if self.state == self.STATE_INITIAL_CONFIG:
-      logging.info("Packets received before initialization, dropping" )
-    elif self.state == self.STATE_ROUTER_LEARNING:
-      self.packet_in_router_learning(dpid, port_id, payload)
-    else:
-      self.packet_in_normal_operation(dpid, port_id, payload)
 
   def packet_in_normal_operation(self, dpid, port_id, payload):
     # Right now, only ARP packets come to the controller
